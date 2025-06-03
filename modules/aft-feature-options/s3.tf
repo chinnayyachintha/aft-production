499a321cb41e97d7344c5cd0cb84ef4c2cabd02b
chinnayyachintha <chinthayadav6@gmail.com>
2025-06-03 16:57:35 +0530

    add filter block to S3 lifecycle rule to resolve provider warning

--- modules/aft-feature-options/s3.tf
+++ modules/aft-feature-options/s3.tf
@@ -44,6 +44,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "aft_logging_bucket_lifecycle_c
     status = "Enabled"
     id     = "aft_logging_bucket_lifecycle_configuration_rule"
 
+    filter {}  # Matches all objects
+
     noncurrent_version_expiration {
       noncurrent_days = var.log_archive_bucket_object_expiration_days
     }
